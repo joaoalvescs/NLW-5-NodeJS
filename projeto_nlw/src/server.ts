b9b355e116e790ea044d42416b849a58e154ca39
João Alves <joaoalves.cs@gmail.com>
2021-04-20 22:19:33 -0300

    Segundo dia de NLW concluído com sucesso!

--- projeto_nlw/src/server.ts
+++ projeto_nlw/src/server.ts
@@ -1,25 +1,12 @@
 import express from "express";
 
+import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-/**
- * GET = Buscas 
- * POST = Criação
- * PUT = Alteração Ex: atualizar alguma informação
- * DELETE : Excluir
- * PATCH: Alterar uma informação específica 
- * Por ex: Alterar somente a senha do usuário, somente uma informação.
- */
+app.use(express.json());
 
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05!",
-    });
-});
-
-app.post("/", (request, response) => {
-    return response.json({ message: "Usuário salvo com sucesso!"})
-})
-
+app.use(routes);
 
 app.listen(3333, () => console.log("Server is running on port 3333"));
